7358c4cf2c39732ce3cb83ca3246605a84ccb739
3amo-magdy <amoramagdymahmoud@gmail.com>
2022-01-03 02:31:57 +0200

    some ui adjustments

--- src/app/components/Q.ts
+++ src/app/components/Q.ts
@@ -4,6 +4,8 @@ export class Q extends IViewable{
     qnumber!:number;
     constructor(id:string,x:number,y:number){
         super(id,x,y);
+        this.qnumber=1081;
+        
     }
     override update(update: any): void {
         this.qnumber+=update as number;
